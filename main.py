7ced329123034b82e7c5fb17a6b4019dca3bb018
lubomir.balaz <lubomirkobalaz@gmail.com>
2024-01-25 23:56:08 +0100

    prototyping

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ test_size = 0.45
 train_data_frame, test_data_frame = train_test_split(data_frame, test_size = test_size, random_state=123456, shuffle=True, stratify=data_frame.iloc[:, -1])
 logger.info("Data frame split into training and test set with test size " + str(test_size))
 
-# Save the data frames to CSV file as output for the next step
+# save the data frames to CSV file as output for the next step
 merged_data_frame = pandas.concat([train_data_frame, test_data_frame])
 merged_data_frame.to_csv('output/merged_data.csv', index=False, sep=';')
 logger.info("Merged data frame saved to output folder.")
@@ -50,5 +50,6 @@ logger.info("Merged data frame saved to output folder.")
 buffer = io.StringIO()
 merged_data_frame.info(buf=buffer)
 logger.info(buffer.getvalue())
+logger.info(merged_data_frame.describe())
 
 logger.info("Data preprocessing pipeline finished.")
