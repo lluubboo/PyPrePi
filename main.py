05585dd67e3dfba89b904db93a3c352d8499f85b
lubomir.balaz <lubomirkobalaz@gmail.com>
2024-01-26 00:29:18 +0100

    prototyping

--- main.py
+++ main.py
@@ -34,25 +34,28 @@ logger.info(data_frame.corr(method='kendall'))
 logger.info("Correlation matrix (Spearman):")
 logger.info(data_frame.corr(method='spearman'))
 
-# prepare reuslting data frame, i want equally distributed values in target column
-bins = numpy.linspace(data_frame.iloc[:, -1].min(), data_frame.iloc[:, -1].max()  + 1e-2, 10)
+# modify resulting data frame
+# the point is to have equally distributed values in the last column in the test part of the data frame
+
+# divide target column to n bins
+bin_count = 6
+bins = numpy.linspace(data_frame.iloc[:, -1].min(), data_frame.iloc[:, -1].max()  + 1e-2, bin_count)
 data_frame['efficiency_bins'] = pandas.cut(data_frame.iloc[:, -1], bins, labels=False)
-data_frame.to_csv('output/data_with_bins.csv', index=False, sep=';')
 bin_counts = data_frame['efficiency_bins'].value_counts()
 valid_bins = bin_counts[bin_counts > 1].index
-data_frame = data_frame[data_frame['efficiency_bins'].isin(valid_bins)]
+data_frame = data_frame[data_frame['efficiency_bins'].isin(valid_bins)] # remove rows with n = 1 bin size
+data_frame = data_frame.dropna(subset=['efficiency_bins']) # remove rows with NaN values in added column
 
+# split the data frame into training and test set
 test_size = 0.2
-data_frame = data_frame.dropna(subset=['efficiency_bins'])
 train_data_frame, test_data_frame = train_test_split(data_frame, test_size = test_size, random_state=123456, shuffle=True, stratify=data_frame['efficiency_bins'])
 logger.info("Data frame split into training and test set with test size " + str(test_size))
 
 # merge the train and test data frames again because of predefined format
 merged_data_frame = pandas.concat([train_data_frame, test_data_frame])
 
-# Remove the 'efficiency_bins' column from the train and test data frames
-train_data_frame = train_data_frame.drop(columns=['efficiency_bins'])
-test_data_frame = test_data_frame.drop(columns=['efficiency_bins'])
+# Remove the 'efficiency_bins' column from the dataset
+merged_data_frame = train_data_frame.drop(columns=['efficiency_bins'])
 
 # save the data frames to CSV file as output for the next step
 merged_data_frame.to_csv('output/merged_data.csv', index=False, sep=';')
